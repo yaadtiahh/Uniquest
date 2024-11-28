3d40e492476fe950d121c53ad6fc645ac37f21c0
Lleanin <Fifashf@yandex.ru>
2024-11-28 23:00:52 +0100

    Fix Gunicorn and project structure

--- Uniquest/settings.py
+++ Uniquest/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,7 +83,7 @@ WSGI_APPLICATION = 'Uniquest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Путь к файлу базы данных
     }
 }
 
--- Uniquest/wsgi.py
+++ Uniquest/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import Uniquest.wsgi
+
 
 from django.core.wsgi import get_wsgi_application
 
